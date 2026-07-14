645c088d1ada6a5b35c588b28ab5d3c8b35dd323
CHRISTIAN A. GALLEGOS YAGUACHI <christian.gallegos@inventar-tech.com>
2026-07-14 10:31:54 -0500

    Primera entidad del clean architecture creada

---
+++ CleanArchitectureTemplate.Domain/Entities/Producto.cs
@@ -0,0 +1,15 @@
+﻿namespace CleanArchitectureTemplate.Domain.Entities
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+    
+        public string Nombre { get; set; } = string.Empty;
+
+        public decimal Precio { get; set; }
+
+        public string? Descripcion { get; set; }
+    
+        public DateTime FechaCreacion { get; set; }
+    }
+}
---
+++ CleanArchitectureTemplate.Infraestructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitectureTemplate.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitectureTemplate.Infraestructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Agregamos los DbSet para nuestras entidades
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
