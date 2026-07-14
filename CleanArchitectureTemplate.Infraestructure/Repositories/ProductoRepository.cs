51180c1ef1575fc42969f68cd1d40771fd5218a8
CHRISTIAN A. GALLEGOS YAGUACHI <christian.gallegos@inventar-tech.com>
2026-07-14 12:01:21 -0500

    Implementacion de la interfaz del repositorio en infraestructura

---
+++ CleanArchitectureTemplate.Infraestructure/Repositories/ProductoRepository.cs
@@ -0,0 +1,54 @@
+﻿using CleanArchitectureTemplate.Domain.Entities;
+using CleanArchitectureTemplate.Domain.Interfaces;
+using CleanArchitectureTemplate.Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitectureTemplate.Infraestructure.Repositories
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductoRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Producto>> GetAllAsync()
+        {
+            return await _context.Productos.ToListAsync();
+        }
+
+        public async Task<Producto?> GetByIdAsync(int id)
+        {
+            return await _context.Productos.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Producto?> GetByNameAsync(string nombre)
+        {
+            return await _context.Productos.FirstOrDefaultAsync(p => p.Nombre == nombre);
+        }
+
+        public async Task AddAsync(Producto producto)
+        {
+            await _context.Productos.AddAsync(producto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Producto producto)
+        {
+            _context.Productos.Update(producto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var producto = await GetByIdAsync(id);
+            if (producto != null)
+            {
+                _context.Productos.Remove(producto);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
