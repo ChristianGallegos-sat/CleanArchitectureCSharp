43dc2c35029010fec4f9d4079a199b960233b15e
CHRISTIAN A. GALLEGOS YAGUACHI <christian.gallegos@inventar-tech.com>
2026-07-14 10:50:57 -0500

    Interaz de producto en el dominio creada

---
+++ CleanArchitectureTemplate.Domain/Interfaces/IProductoRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitectureTemplate.Domain.Entities;
+
+namespace CleanArchitectureTemplate.Domain.Interfaces
+{
+    public interface IProductoRepository
+    {
+        Task<IEnumerable<Producto>> GetAllAsync();
+
+        Task<Producto?> GetByIdAsync(int id);
+
+        Task<Producto?> GetByNameAsync(string nombre);
+
+        Task AddAsync(Producto producto);
+
+        Task UpdateAsync(Producto producto);
+
+        Task DeleteAsync(int id);
+    }
+}
